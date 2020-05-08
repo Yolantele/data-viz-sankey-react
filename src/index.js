8b352751d60addf75c678ed2225cb84436462874
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2020-05-08 11:03:20 +0100

    takes away interface to render pure modal

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import { Layout, Card, Typography } from 'antd'
+import { Card } from 'antd'
 import './styles.css'
 import wasteData from './data/geoFluxusData'
 import { render } from 'react-dom'
 import React from 'react'
 import { CircularExample } from './views'
 
-const { Header, Footer, Content } = Layout
-
 const App = () => (
-  <div>
-    <Header />
-    <Content style={{ margin: 40 }}>
-      <Card title='Circular Waste Data' style={{ margin: 20, backgroundColor: 'darkslategrey' }}>
-        <CircularExample data={wasteData} width={900} height={1000} />
-      </Card>
-    </Content>
-
-    <Footer style={{ height: 200 }} />
-  </div>
+  <Card style={{ backgroundColor: 'darkslategrey' }}>
+    <CircularExample data={wasteData} width={900} height={1000} />
+  </Card>
 )
 
 render(<App />, document.getElementById('root'))
